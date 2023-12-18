69c3ab1a5607b8e90e8830253dbdcdb0a63bdfcf
AlexandreMisrahi2005 <alex.misrahi@icloud.com>
2023-12-18 13:04:00 +0100

    fix mapping for our data

--- generate_mapping.py
+++ generate_mapping.py
@@ -1,6 +1,8 @@
+from email.mime import image
 import os, glob
 import json
 import argparse
+import warnings
 
 
 def public_paths_labeled(root):
@@ -31,25 +33,31 @@ def public_paths_labeled(root):
 def official_paths_labeled(root):
     """Map paths for official labeled datasets as dictionary list"""
 
-    image_path = os.path.join(root, "Official/Train_Labeled/images/*")
-    label_path = os.path.join(root, "Official/Train_Labeled/labels/*")
+    image_path = os.path.join(root, "YeaZ/images/*")
+    label_path = os.path.join(root, "YeaZ/labels/*")
 
     images_raw = sorted(glob.glob(image_path))
     labels_raw = sorted(glob.glob(label_path))
 
     data_dicts = []
 
-    for image_path, label_path in zip(images_raw, labels_raw):
-        name1 = image_path.split("/")[-1].split(".")[0]
-        name2 = label_path.split("/")[-1].split("_label")[0]
-        assert name1 == name2
+    images_raw = [(lambda x: ".".join(x.split("/")[-1].split(".")[:-1]))(x) for x in images_raw]
+    labels_raw = [(lambda x: x.split("/")[-1].split("_label")[0])(x) for x in labels_raw]
+    
+    count_mismatch = 0
+    for name1 in images_raw:
+        # assert name1 == name2, f"{name1} does not match {name2}"
+        if not (name1 in labels_raw):
+            count_mismatch += 1
+            warnings.warn(f"{name1} found no match")
+        else:
+            data_item = {
+                "img": "Datasets/YeaZ/images/"+name1+".tiff",
+                "label": "Datasets/YeaZ/labels/"+name1+"_label.tiff",
+            }
 
-        data_item = {
-            "img": image_path.split("CellSeg/")[-1],
-            "label": label_path.split("CellSeg/")[-1],
-        }
-
-        data_dicts.append(data_item)
+            data_dicts.append(data_item)
+    print(count_mismatch, " mismatches")
 
     map_dict = {"official": data_dicts}
 
